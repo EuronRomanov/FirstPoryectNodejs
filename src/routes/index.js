2c1a229e4d47eba32e829061406cecc576774195
EuronRomanov <juanpablo.gaona@yahoo.com>
2020-09-17 20:00:34 -0500

    Add views

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,11 +4,11 @@ const router = express.Router();
 
 router.get('/',(req, res)=>{
     //res.sendFile(path.join(__dirname,'views/index.html'));
-   res.render('index', { title: 'First Web'});
+   res.render('index.html', { title: 'First Web'});
  });
 
 
  router.get('/contact', (req, res)=>{
-    res .render('contact', {title: 'Page contact'});
+    res .render('contact.html', {title: 'Page contact'});
  });
  module.exports = router;
